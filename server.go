398196fe32394acdec3a08db597b2eb511371dc0
Bastien <bastienb.balaud@gmail.com>
2019-10-17 21:49:52 +0200

    rework to prevent some nasty thing

--- server.go
+++ server.go
@@ -6,6 +6,7 @@ import (
  "github.com/alexflint/go-arg"
  "log"
  "net"
+ "time"
 )
 
 func main() {
@@ -15,12 +16,18 @@ func main() {
  args.Port = 8080
  arg.MustParse(&args)
  fmt.Println("Listen on port", args.Port)
+ srv := &http.Server{
+    Addr: ":8080",
+    ReadTimeout: 5 * time.Second,
+    WriteTimeout: 10 * time.Second,
+}
 
 
  http.HandleFunc("/", rootPage)
  http.HandleFunc("/ip",ipPage)
- // server_info := fmt.Sprintf(":%s", strconv.Itoa(args.Port))
- log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", args.Port), nil))
+ http.HandleFunc("/ua", uaPage)
+ http.HandleFunc("/header", headerPage)
+ log.Fatal(srv.ListenAndServe())
 }
 
 func rootPage(w http.ResponseWriter, r *http.Request) {
@@ -32,7 +39,14 @@ func ipPage(w http.ResponseWriter, r *http.Request){
  ip,_,_ := net.SplitHostPort(r.RemoteAddr)
  w.Write([]byte(ip))
 }
-
+func uaPage(w http.ResponseWriter, r *http.Request){
+ response := "Your user Agent is "+ r.Header.Get("User-Agent") + "\n"
+ w.Write([]byte(response))
+}
+func headerPage(w http.ResponseWriter, r *http.Request){
+ response := "Your user Agent is "+ r.Header.Get("") + "\n"
+ w.Write([]byte(response))
+}
 func getIp(r *http.Request) (string){
   ip,_,_ := net.SplitHostPort(r.RemoteAddr)
   return ip
